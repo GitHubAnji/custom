73967a39f2fb1879026fafb54456684d96d08f7a
anji <anji@gmail.com>
2017-01-14 15:58:14 +0530

    My INITIAL COMMIT

---
+++ AR/ARinitialdata/src/com/ar/initialdata/constants/ARInitialDataConstants.java
@@ -0,0 +1,25 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.ar.initialdata.constants;
+
+/**
+ * Global class for all ARInitialData constants.
+ */
+public final class ARInitialDataConstants extends GeneratedARInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "ARinitialdata";
+
+	private ARInitialDataConstants()
+	{
+		//empty
+	}
+}
